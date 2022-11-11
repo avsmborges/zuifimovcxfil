ec2cfd4737eeeb1b48f367f10fe597df38f44734
alexandre.borges@dellavolpe.com.br <alexandre.borges@dellavolpe.com.br>
2022-11-11 09:57:14 +0000

    Carga Inicial

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("br.com.dellavolpe.zuifimovcxfil.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
---
+++ webapp/ext/controller/ListReportExt.controller.js
@@ -0,0 +1,29 @@
+sap.ui.define([],
+    function () {
+        "use strict";
+        return {
+            openAppTab: function (oEvt) {
+                var oNavigationController = this.extensionAPI.getNavigationController();
+                oNavigationController.navigateExternal("Action-" + oEvt.getSource().getId().substring(oEvt.getSource().getId().lastIndexOf("--") + 2));
+            },
+
+            _refreshTable: function () {
+                try {
+                    if (this._bSearch) {
+                        this.extensionAPI.refreshTable();
+                    }
+                } catch (e) {
+                    //--
+                }
+                var refreshTable = function () { this._refreshTable() }.bind(this);
+                setTimeout(refreshTable, 30000);
+            },
+
+            onInit: function () {
+                this._refreshTable(this);
+            },
+            onBeforeRebindTableExtension: function () {
+                this._bSearch = true;
+            }
+        };
+    });
